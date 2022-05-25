f4c6e4c92ae731b117e254f4385ee00c17e3e67c
kritikapoor04 <kritikapoor2401@gmail.com>
2022-05-26 02:24:11 +0530

    added

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
+from django.db.models import Avg
 
 from .models import Book
 
@@ -7,8 +8,13 @@ from .models import Book
 
 def index(request):
     books = Book.objects.all()
+    num_books= books.count()
+    avg_rating=books.aggregate(Avg("rating"))
+
     return render(request,"book_outlet/index.html",{
-        "books": books
+        "books": books,
+        "total_books":num_books,
+        "avg_rating":avg_rating
     })
 
 def book_detail(request, slug):
