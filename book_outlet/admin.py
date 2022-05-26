630f223c132e32d91ed6d85dd7509080f753b1e5
kritikapoor04 <kritikapoor2401@gmail.com>
2022-05-27 01:16:43 +0530

    added

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,6 +1,6 @@
 from xml.dom.minidom import ReadOnlySequentialNamedNodeMap
 from django.contrib import admin
-from .models import Book
+from .models import Book,Author
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
@@ -9,3 +9,4 @@ class BookAdmin(admin.ModelAdmin):
     list_display=("title","author")
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
